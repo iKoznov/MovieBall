961e3caada48bae78f1c0fd1a3e6421dbc563d28
IGOR KOZNOV <igorkoznov@IGORs-MacBook-Air.local>
2014-07-06 09:16:26 +0400

    iOS cmake integration in process

--- xcode-cmake-lib/MovieBall/MovieBall/AppDelegate.m
+++ xcode-cmake-lib/MovieBall/MovieBall/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "hello.h"
 
 @implementation AppDelegate
 
@@ -16,6 +17,9 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+	
+	hello();
+	
     return YES;
 }
 

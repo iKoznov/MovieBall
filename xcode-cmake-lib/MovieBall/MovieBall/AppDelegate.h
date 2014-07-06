1a6c664d69a383a040571867cc1aa8f7f89542ae
IGOR KOZNOV <igorkoznov@IGORs-MacBook-Air.local>
2014-07-06 08:13:53 +0400

    experimenting with cmake library building with Xcode automatically

---
+++ xcode-cmake-lib/MovieBall/MovieBall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MovieBall
+//
+//  Created by IGOR KOZNOV on 06.07.14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

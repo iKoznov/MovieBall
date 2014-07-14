abd8dc1e2476f4fc9212da89027e0d8ee0fcc7ca
IGOR KOZNOV <igorkoznov@IGORs-MacBook-Air.local>
2014-07-14 14:44:08 +0400

    copy cmake find modules

--- xcode-cmake-lib/engine/test/main.cpp
+++ xcode-cmake-lib/engine/test/main.cpp
@@ -9,11 +9,13 @@ BOOST_AUTO_TEST_SUITE(CalculatorTests)
 BOOST_AUTO_TEST_CASE(PlusTest)
 {
     BOOST_CHECK_EQUAL(1 + 2, 3);
+	int *b = new int;
 }
 
 BOOST_AUTO_TEST_CASE(MinusTest)
 {
     BOOST_CHECK_EQUAL(2 - 1, 1);
+	int *a = new int[100];
 }
 
 BOOST_AUTO_TEST_SUITE_END()
